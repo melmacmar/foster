f88a649377d74ea30291f422846e5a9ba0d9720a
Melanie Marshall <melmacmar@gmail.com>
2016-04-22 14:57:53 +0000

    trying to commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foster::Application.config.secret_key_base = 'f1b66e6dbdc9855c160fd5fd75e6bcff94a4c86f11cc3581cfb857c04dc6c6b0dab5ba20ed430390b723b5910c5cfb3d955540292aeb6f778ed11055f353f7e1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Foster::Application.config.session_store :cookie_store, key: '_foster_session'
