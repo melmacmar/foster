37ea9e43cf08c1acb0997324b92c21aec6609af7
Melanie Marshall <melmacmar@gmail.com>
2016-04-25 08:52:35 +0000

    Added Teach a Course form and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Foster::Application.routes.draw do
   devise_for :users
     root 'static_pages#index'
+    namespace :instructor do
+      resources :courses, only: [:new, :create, :show]
+    end
 
   
 
