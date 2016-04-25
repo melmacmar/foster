6460e007f7f4aa8f9b3b474b0672974fd15be9d8
Melanie Marshall <melmacmar@gmail.com>
2016-04-25 10:39:44 +0000

    Added Section button

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ devise_for :users
   root 'static_pages#index'
     resources :courses, only: [:index, :show]
   namespace :instructor do
-    resources :courses, only: [:new, :create, :show]
-  end
+    resources :courses, only: [:new, :create, :show] do
+      resources :sections, only: [:new, :create]
+      end
+    end
+
 
 
   
