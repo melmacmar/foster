b1a8712f5e5452228c7f3c2bbef7fb314625a325
Melanie Marshall <melmacmar@gmail.com>
2016-04-25 12:03:11 +0000

    Added something

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
-Foster::Application.routes.draw do
-devise_for :users
+Foster::Application.routes.draw do 
+  devise_for :users
   root 'static_pages#index'
-    resources :courses, only: [:index, :show]
+  resources :courses, only: [:index, :show]
   namespace :instructor do
+    resources :sections, only: [] do
+      resources :lessons, only: [:new, :create]
+    end
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:new, :create]
-      end
     end
-
-
+  end
+end
 
   
 
@@ -66,4 +68,4 @@ devise_for :users
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
