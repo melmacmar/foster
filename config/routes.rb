369422b340d6741434a28f383347651ac0841d12
Melanie Marshall <melmacmar@gmail.com>
2016-05-10 14:11:15 +0000

    Trying to fix runtime error

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,17 @@ Foster::Application.routes.draw do
   devise_for :users
   resource :dashboard, only: [:show]
   root 'static_pages#index'
-   resources :courses, only: [:index, :show] do
+  resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
-    resources :lessons, only: [:show]
-    namespace :instructor do
+  resources :lessons, only: [:show]
+  namespace :instructor do
     resources :lessons, only: [:update]
-    resources :sections, only: [] do
-    resources :lessons, only: [:new, :create]
-  end
+    resources :sections, only: [:update] do
+      resources :lessons, only: [:new, :create]
+    end
     resources :courses, only: [:new, :create, :show] do
-     resources :sections, only: [:create]
+      resources :sections, only: [:create]
     end
   end
 end
