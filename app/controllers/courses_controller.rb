a21fc738c014595d2e11c1ee5e1d0805e1729b1c
Melanie Marshall <melmacmar@gmail.com>
2016-05-10 19:02:40 +0000

    Added Ken's fixes

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -16,9 +16,9 @@ class Instructor::CoursesController < ApplicationController
     end
   end
 
-  def show
-    @section = Section.new
-  end
+def show
+  @section=Section.new
+end
 
   private
   def require_authorized_for_current_course
