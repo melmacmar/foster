811f8aabddcb651b8c60867546e1b95c3ce35a4d
Melanie Marshall <melmacmar@gmail.com>
2016-05-12 14:59:41 +0000

    Trying to fix course/index error

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,4 +1,4 @@
-class Instructor::CoursesController < ApplicationController
+class CoursesController < ApplicationController
   before_action :authenticate_user!
   before_action :require_authorized_for_current_course, only: [:show]
 
